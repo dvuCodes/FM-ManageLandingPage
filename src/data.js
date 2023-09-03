ff4672c38f213b0264fb17b907df5a2686ab24dc
Dat-Vu <89580762+dvuCodes@users.noreply.github.com>
2023-09-03 12:23:47 +1000

    Added the testimony section however we're having trouble with the pagination for the image carusell

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    image: ".../../src/images/avatar-ali.png",
+    name: "Ali Bravo",
+    testimony:
+      "lorem epsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+  },
+  {
+    image: "../../src/images/avatar-anisha.png",
+    name: "Anisha Li",
+    testimony:
+      "lorem epsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+  },
+  {
+    image: "../../src/images/avatar-richard.png",
+    name: "Richard Watts",
+    testimony:
+      "lorem epsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+  },
+];
