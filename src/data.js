28cfd84c19b94248a66e04dcea9c9adceb6dd306
Dat-Vu <89580762+dvuCodes@users.noreply.github.com>
2023-09-05 19:41:24 +1000

    moved images folder to public

--- src/data.js
+++ src/data.js
@@ -1,18 +1,18 @@
 export default [
   {
-    image: ".../../images/avatar-ali.png",
+    image: "/images/avatar-ali.png",
     name: "Ali Bravo",
     testimony:
       "lorem epsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
   },
   {
-    image: "../../images/avatar-anisha.png",
+    image: "/images/avatar-anisha.png",
     name: "Anisha Li",
     testimony:
       "lorem epsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
   },
   {
-    image: "../../images/avatar-richard.png",
+    image: "/images/avatar-richard.png",
     name: "Richard Watts",
     testimony:
       "lorem epsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
